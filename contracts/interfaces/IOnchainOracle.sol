bcc87d321abd5ecd5c36a07c12c4255db007e314
MEDISETTI LAKSHMAN VENKATA PHANI <198173010+phani2582@users.noreply.github.com>
2026-07-18 07:28:10 +0530

    Delete contracts directory

--- contracts/interfaces/IOnchainOracle.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-interface IOnchainOracle {
-    function price(address pool) external view returns (uint256 price, uint256 feeBps);
-    function priceWithImpact(address pool, uint256 amountIn) external view returns (uint256 price, uint256 feeBps);
-    function feeBpsBase() external view returns(uint256);
-}
